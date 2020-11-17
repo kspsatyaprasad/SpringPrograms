d4dbd1b546ed176b97cdc2689b7c2b4a2e499772
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 22:05:03 +0530

    added code by using primary annotation to remove the dilemma between components

---
+++ BaiscSpringAnnotationPrograms/SpringAnnotations/src/main/java/com/SpringPrograms/SpringAnnotations/Mediatek.java
@@ -0,0 +1,17 @@
+package com.SpringPrograms.SpringAnnotations;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class Mediatek implements MobileProcessor
+{
+
+	public void process() 
+	{
+		System.out.println("Not The Best CPU");
+     		
+	}
+
+}
