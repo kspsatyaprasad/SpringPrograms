c5d3f80dba5f40b9685d1a0d66db826d9d8cc9d3
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 14:06:52 +0530

    added code for perfoorming dependency injection and created xml file

--- BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
@@ -1,9 +1,14 @@
 package com.SpringPrograms.BasicPrograms;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Vehicle obj = new Bike();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//we can also BeanFactory which is subset of ApplicationContext
+    	Vehicle obj = (Vehicle)context.getBean("vehicle"); //gives the object of the available class
         obj.drive();
     }
 }
