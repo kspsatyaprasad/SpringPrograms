d2569e419e38df65a701e6e90060f60e9110aa65
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 12:53:45 +0530

    created another class to perform dependency injection

--- BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
@@ -3,7 +3,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Car car = new Car();
-        car.drive();
+        Bike bike = new Bike();
+        bike.drive();
     }
 }
---
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/Bike.java
@@ -0,0 +1,9 @@
+package com.SpringPrograms.BasicPrograms;
+
+public class Bike {
+	public void drive()
+	{
+		System.out.println("Bike is Running");
+	}
+
+}
