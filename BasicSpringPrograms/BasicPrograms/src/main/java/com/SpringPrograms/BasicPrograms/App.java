abc600889c00b196971e78a413466c3696098160
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 12:49:35 +0530

    added basic code with out using dependency injection

--- BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
@@ -1,13 +1,9 @@
 package com.SpringPrograms.BasicPrograms;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car car = new Car();
+        car.drive();
     }
 }
---
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/Car.java
@@ -0,0 +1,9 @@
+package com.SpringPrograms.BasicPrograms;
+
+public class Car {
+	public void drive()
+	{
+		System.out.println("car is Running");
+	}
+
+}
