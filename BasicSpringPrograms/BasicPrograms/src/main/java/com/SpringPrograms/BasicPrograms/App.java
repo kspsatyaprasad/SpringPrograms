cca90f16a750239e2f9d3b45dad269eeaa91be5e
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 15:15:55 +0530

    added code for setter injection and refactored

--- BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
+++ BasicSpringPrograms/BasicPrograms/src/main/java/com/SpringPrograms/BasicPrograms/App.java
@@ -8,8 +8,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");//we can also BeanFactory which is subset of ApplicationContext
-//    	Vehicle obj = (Vehicle)context.getBean("bike"); //gives the object of the available class
-//      obj.drive();
+
      	Tyre t = (Tyre)context.getBean("tyre"); 
         System.out.println(t);
     }
