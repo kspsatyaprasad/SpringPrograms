fb9b3c63b63d97a38a74a5e0a5252cb90a208049
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-17 15:51:30 +0530

    added basic code without using dependency injection

---
+++ BaiscSpringAnnotationPrograms/SpringAnnotations/src/main/java/com/SpringPrograms/SpringAnnotations/App.java
@@ -0,0 +1,14 @@
+package com.SpringPrograms.SpringAnnotations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Samsung s7 = new Samsung();
+        s7.config();
+    }
+}
---
+++ BaiscSpringAnnotationPrograms/SpringAnnotations/src/main/java/com/SpringPrograms/SpringAnnotations/Samsung.java
@@ -0,0 +1,11 @@
+package com.SpringPrograms.SpringAnnotations;
+
+public class Samsung 
+{
+	public void config()
+	{
+	   System.out.println("Octa Core,4gb RAm,12MP camera");	
+	}
+	
+
+}
